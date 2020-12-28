139ba5fb076d17c77670d662b68120a38f14d7ce
Peter Zhang <petejzh@gmail.com>
2020-12-28 17:35:13 -0500

    work

--- article.md
+++ article.md
@@ -1,5 +1,13 @@
 
 
+plan of attack:
+
+state-level correlation
+
+graph
+
+county-level correlations
+
 
 
 
@@ -11,3 +19,8 @@ https://github.com/nytimes/covid-19-data
 https://www.statista.com/statistics/183588/population-density-in-the-federal-states-of-the-us/
 
 https://coronavirus.jhu.edu/testing/states-comparison
+
+https://www.kff.org/other/state-indicator/distribution-by-raceethnicity/?currentTimeframe=0&sortModel=%7B%22colId%22:%22Location%22,%22sort%22:%22asc%22%7D
+
+https://fred.stlouisfed.org/release/tables?rid=249&eid=259515
+
--- regression.R
+++ regression.R
@@ -1,15 +1,31 @@
-install.packages("jtools")
 library(jtools)
 
 data <- read.csv("sources/covid.csv")
 data$Income = as.numeric(data$Income)
+data$Density = as.numeric(data$Density)
+data$Testing = as.numeric(data$Testing)
 
 data$deathIncrease = (data$Oct_2020_Deaths-data$June_2020_Deaths)/data$Total.Population*100000
+data$caseIncrease = (data$Oct_2020_Cases-data$June_2020_Cases)/data$Total.Population*100000
 data$Oct_2020_DeathRate = data$Oct_2020_Deaths/data$Total.Population*100000
+data$Oct_2020_CaseRate = data$Oct_2020_Cases/data$Total.Population*100000
 cor(data$deathIncrease, data$Biden_Margin)
 cor(data$deathIncrease, data$Income)
 data$Boomers = data$Total.number..adults.age.65.and.older/data$Total.Population
 
-linear.1 <- lm(Oct_2020_DeathRate ~ Biden_Margin + Income + Boomers,
+
+linear.1 <- lm(Oct_2020_CaseRate ~ Biden_Margin+ Density + Testing + Income + Boomers + Black + White + Asian + Hispanic,
+               data=data)
+linear.1 <- lm(Oct_2020_DeathRate ~ Biden_Margin+ Density + Income + Boomers + Black + White + Asian + Hispanic,
+               data=data)
+linear.1 <- lm(deathIncrease ~ Biden_Margin,
                data=data)
 summ(linear.1)
+
+
+countydata <- read.csv("sources/matched.csv")
+countydata$SeptCases = (countydata$Cases.9.30.2020-countydata$Cases.8.31.2020 -(countydata$Cases.7.31.2020-countydata$Cases.6.30.2020))/countydata$Population_2019*100000
+countydata$Population.Density
+linear.2 = lm(Cases.9.30.2020 ~ diff + Population.Density,
+              data = countydata)
+summ(linear.2)
