cf8e5ed39fbb4ffc8ea3c5ffdb93af03eddd6202
Peter Zhang <petejzh@gmail.com>
2021-01-04 14:13:29 -0500

    move county analysis

--- county_analysis.R
+++ county_analysis.R
@@ -1,12 +1,35 @@
-library(jtools)
 
+# case correlation
+cor(state_data$Biden.Margin, state_data$change_caserate)
+
+# death correlation
+cor(state_data$Biden.Margin, state_data$change_deathrate)
+
+# read county-level data
 county_data <- read.csv("agglomerated/county_dataset.csv")
-county_data$caserate = county_data$X12.31.2020_cases/county_data$population*100000
+
+# create summary table
+kable(summarize(county_data))
+
+
+county_data$caserate = (county_data$X12.31.2020_cases-county_data$X11.30.2020_cases)/county_data$population*100000
 county_data$deathrate = county_data$X12.31.2020_deaths/county_data$population*100000
 county_data$elderly = county_data$X60older/county_data$population
 county_data$icu_rate = county_data$icus/county_data$population
 
-county_data
+
+ggplot(data = county_data,
+       aes(x = per_dem_2020 - per_gop_2020,
+           y = caserate,
+           color = per_dem_2020 - per_gop_2020)) +
+  geom_point() +
+  ggtitle("Change in Death Rate")+
+  xlab("Biden Margin") +
+  ylab("Deaths per 100K") +
+  scale_color_gradient2(name="Biden Margin",low = "red", mid="grey", high = "blue",  midpoint = 0) +
+  theme(legend.position = "none") +
+  geom_smooth(method="lm", se=F, col="black")
+
 linear.1 <- lm(dem_caserate~ per_dem_2020 + maskuse + X60older/population + density + total_deaths/population + lattitude + distance_to_work + per_capita_income + median_household + black + hispanic + bachelors + poverty + tests,
                data=county_data)
 summ(linear.1)
@@ -24,13 +47,6 @@ ggplot(county_data,
   geom_point() +
   geom_smooth(method="lm", se=F, col="black")
 
-
-
-library(ggiraph)
-library(ggiraphExtra)
-library(plyr)
-library(ggplot2)
-ggPredict(linear.2,se=TRUE,interactive=TRUE)
 
 
 
